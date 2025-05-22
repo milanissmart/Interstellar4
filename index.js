50111b4f4ae2d7a113225626d6524eef3967f101
milanissmart <milanmoturu@gmail.com>
2025-05-22 15:42:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -142,4 +142,4 @@ server.on("listening", () => {
   console.log(chalk.green(`🌍 Server is running on http://localhost:${PORT}`));
 });
 
-server.listen({ port: PORT });
+server.listen({ port: PORT, host: '0.0.0.0' });
